341d6b1ccc552f192f244dbdcf1cd72fce368862
CHECHO-c <baloncestoentablon@gmail.com>
2025-09-04 17:32:25 -0500

    Laravel curso

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <x-container>
+        <x-card class="mb-4">
+            {{ __("Estas logueado") }}
+        </x-card>
+        <x-card>
+            {{ __("Estas logueado") }}
+        </x-card>
+    </x-container>
+    
+</x-app-layout>
