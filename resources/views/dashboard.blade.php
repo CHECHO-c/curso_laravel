eb6e98d8cd518d740aa5d2b6f1339ee837b28fde
CHECHO-c <baloncestoentablon@gmail.com>
2025-09-12 12:05:54 -0500

    Agregacion de posts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,30 @@
 <x-app-layout>
     <x-container>
+        <form action="{{ route('posts.store') }}" class="px-4 mb-8" method="post">
+            @csrf
+            <textarea 
+                name="body" 
+                rows="2" 
+                placeholder="Comentanos algo..."
+                class="w-full mb-2 text-black bg-transparent border-0 border-b-2 border-slate-800 focus:ring-0 focus:outline-none resize-none overflow-hidden placeholder-gray-500"
+            ></textarea>
+
+
+            <input 
+                type="submit"
+                
+                class="px-4 py-2 p-0 bg-yellow-400  text-gray-800 font-semibold sm:rounded-lg text-xs"
+                
+            >
+        </form>
+
+
         @foreach ($posts as $post )
         <a href="" class="">
             {{ $post->user->name }}
         </a>
             <x-card>
-                <h1>Id: {{ $post->id }}</h1>
+                
                 {{ $post->body}}
                 <div class="text-sm text-slate-500">
                     {{ $post->created_at->diffForHumans() }}
